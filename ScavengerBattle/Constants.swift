85e9dd94bb679caeb3e5315f058e76b1c9c1ae2f
RobertWais <wais.robert@uwlax.edu>
2018-10-23 23:52:20 -0500

    SignInVc UI started

---
+++ ScavengerBattle/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ScavengerBattle
+//
+//  Created by Robert Wais on 10/23/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+struct Constants {
+    struct Colors {
+        static let baseColor = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1.0)
+    }
+}
--- ScavengerBattle/ViewController.swift
+++ ScavengerBattle/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guestNameField: UITextField!
+    @IBOutlet weak var startBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDelegate();
+        startBtn.layer.borderWidth = 2.0
+        startBtn.layer.borderColor = UIColor.white.cgColor
+        startBtn.layer.cornerRadius = startBtn.frame.height/2
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func startBtnPressed(_ sender: Any) {
     }
+    
+}
 
-
+extension ViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func setDelegate(){
+        self.guestNameField.delegate = self
+    }
+    
 }
 
